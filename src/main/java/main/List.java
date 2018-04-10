fcd3e1ac9b64f82a853ac6dd41b7c87d1dc4b7f9
Anastasios Palatiou <anastasios.palatiou@haw-hamburg.de>
2018-04-10 08:45:31 +0200

    add List

--- src/main/java/main/List.java
+++ src/main/java/main/List.java
@@ -1,5 +1,18 @@
 package main;
 
-public interface List {
+public interface List<T> {
 
+    int length();
+
+    boolean insert(T elem, int pos) throws IllegalArgumentException;
+
+    T delete(int pos) throws IllegalArgumentException;
+
+    T touch(int pos) throws IllegalArgumentException;
+
+    boolean clear();
+
+    List<T> concat(List<T> otherList) throws IllegalArgumentException;
+
+    List<T> substitute(List<T> subList) throws IllegalArgumentException;
 }

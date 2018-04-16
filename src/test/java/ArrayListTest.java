8b5bbf6c7faab82c01a1b243a91efa1416235709
Anastasios Palatiou <anastasios.palatiou@haw-hamburg.de>
2018-04-16 15:55:38 +0200

    push

--- src/test/java/ArrayListTest.java
+++ src/test/java/ArrayListTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-public class ListTest {
+public class ArrayListTest {
 
     private ArrayList<Integer> arrayListTestAry;
     private ArrayList<Integer> arrayListEmptyAry;
